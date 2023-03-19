87449784aca89e3604a4b9a6ed69b918ef6d7ac0
Alfredoeb9 <alfredoeb96@gmail.com>
2023-03-18 23:01:54 -0700

    [ads] placed ad code

--- src/components/ad/AmazonAd.jsx
+++ src/components/ad/AmazonAd.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import postscribe from "postscribe";
 import Helmet from "react-helmet";
 
 //! Not Working Don't Push or Deploy
@@ -9,6 +10,10 @@ const AmazonAdd = () => {
   const [amazonSrc2, setamazonSrc2] = useState("");
 
   useEffect(() => {
+    postscribe(
+      "#amzn-assoc-ad-b14a52ba-f95b-4330-838f-47deab2cec43",
+      '<script async src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=b14a52ba-f95b-4330-838f-47deab2cec43"></script>'
+    );
     setamazonSrc2(
       '<div class="alignleft"> <script type="text/javascript"> amzn_assoc_ad_type = "banner"; amzn_assoc_marketplace = "amazon"; amzn_assoc_region = "US"; amzn_assoc_placement = "assoc_banner_placement_default"; amzn_assoc_campaigns = "kuft"; amzn_assoc_banner_type = "category"; amzn_assoc_p = "48"; amzn_assoc_isresponsive = "false"; amzn_assoc_banner_id = ID; amzn_assoc_width = "728"; amzn_assoc_height = "90"; amzn_assoc_tracking_id = "reater-20"; amzn_assoc_linkid = "asoc_linked"; </script> <script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=b14a52ba-f95b-4330-838f-47deab2cec43"></script> </div>'
     );
@@ -20,9 +25,9 @@ const AmazonAdd = () => {
 
   return (
     <div className="amz-ad-container">
-      <Helmet>
+      {/* <Helmet>
         <script className="amazonSrc" async src={amazonSrc} />
-      </Helmet>
+      </Helmet> */}
       <div
         // dangerouslySetInnerHTML={{ __html: amazonSrc }}
         id="amzn-assoc-ad-b14a52ba-f95b-4330-838f-47deab2cec43"
