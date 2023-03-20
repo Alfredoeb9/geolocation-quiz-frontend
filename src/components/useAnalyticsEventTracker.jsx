11782c3e78f6e58f2714dcc962d61e88f83f3f72
Alfredo <alfredoeb96@gmail.com>
2023-03-20 08:41:24 -0700

    [analytics] changed react-ga to new npm package

--- src/components/useAnalyticsEventTracker.jsx
+++ src/components/useAnalyticsEventTracker.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 const useAnalyticsEventTracker = (category = "Blog category") => {
   const eventTracker = (action = "test action", label = "test label") => {
