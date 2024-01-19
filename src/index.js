2420395dd0425dbdf5416fa680403ee469f6e38e
Alfredoeb9 <alfredoeb96@gmail.com>
2024-01-18 20:45:25 -0800

    [packages] updated packages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import persistStore from "redux-persist/es/persistStore";
 import { store } from "./app/store";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const persistor = persistStore(store);
 
@@ -23,9 +20,4 @@ root.render(
       </PersistGate>
     </Provider>
   </>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
